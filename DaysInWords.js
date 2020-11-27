d2012a193789d0941002fad8f6a84ae6a734bd7c
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-27 23:26:21 +0530

    Added: Code to take input as digits and output as words using switch case statement

--- DaysInWords.js
+++ DaysInWords.js
@@ -46,3 +46,22 @@ let number=Number(prompt("Enter the number(0-9) :"));
         default :   console.log("Wrong input.");
     }
 
+// 3)Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
+{
+    let number=Number(prompt("Enter any number(1,10,100,..,100000):"));
+    switch(number){
+        case 1  :  console.log("Unit");
+                    break; 
+        case 10  :  console.log("Ten");
+                    break; 
+        case 100  :  console.log("Hundread");
+                    break; 
+        case 1000  :  console.log("Thousand");
+                    break; 
+        case 10000  :  console.log("Ten thousand");
+                    break; 
+        case 100000  :  console.log("Lakh");
+                    break; 
+        default :   console.log("Wrong input");
+    }
+}
