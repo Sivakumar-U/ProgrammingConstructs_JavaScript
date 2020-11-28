374159350487f8a2b00c355688110e70af3bf332
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-28 17:00:43 +0530

    Added: Code to check given year is leap year or not

--- if-else.js
+++ if-else.js
@@ -61,3 +61,11 @@ if((month.toLowerCase() === 'march' && date > 20 && date< 31)
     console.log("True.");
 else
     console.log("False.");
+
+// 3) Write a program that takes a year as input and outputs the Year is a Leap Year or not a Leap Year. A Leap Year checks for 4 Digit Number, Divisible by 4 and not 100 unless divisible by 400.
+let year=Number(prompt("Enter year :"));
+if( ((year % 4) == 0 && (year%100) != 0) || year% 400 == 0)
+    console.log("Leap year.");
+else
+    console.log("Not a leap year.");
+
