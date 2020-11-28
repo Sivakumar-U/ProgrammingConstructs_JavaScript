e115cb97fa339dd4c6319af5f6ec485b14335b1b
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-28 20:41:07 +0530

    Added: Code to read a number and display the week day

--- IfElse_IfAndElse.js
+++ IfElse_IfAndElse.js
@@ -16,3 +16,24 @@ else if(number == 5)
 else
     console.log("Wrong input.");
 }
+
+// 2) Read a Number and Display the week day (Sunday, Monday,...)
+{
+    let day=Number(prompt("Enter week day number :"));
+    if(day == 1)
+        console.log("Sunday.");
+    else if(day == 2)
+        console.log("Monday.");
+    else if(day == 3)
+        console.log("Tuesday.");
+    else if(day == 4)
+        console.log("Wednesday.");
+    else if(day == 5)
+        console.log("Thursday.");
+    else if(day == 6)
+        console.log("Friday.");
+    else if(day == 7)
+        console.log("Saturday.");
+    else
+        console.log("Wrong input.");
+}
