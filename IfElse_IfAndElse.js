a272365a847e835910e00898f13926b872688106
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-28 21:42:39 +0530

    Added: Code to enter 3 numbers to do arithmatic operations and find the maximum and minimum values

--- IfElse_IfAndElse.js
+++ IfElse_IfAndElse.js
@@ -52,3 +52,29 @@ else
     else
         console.log("Wrong input.");
 }
+
+// 4)Enter 3 Numbers do following arithmetic operation and find the one that is maximum and minimum
+// 1. a + b * c 3. c + a / b
+// 2. a % b + c 4. a * b + c
+{
+    let a=Number(prompt("Enter value a :"));
+    let b=Number(prompt("Enter value b :"));
+    let c=Number(prompt("Enter value c :"));
+    
+    let operation1=a+b*c;
+    let operation2=a%b+c;
+    let operation3=c+a/b;
+    let operation4=a*b+c;
+
+    console.log(operation1);
+    console.log(operation2);
+    console.log(operation3);
+    console.log(operation4);
+    
+    let max=(operation1 > operation2 && operation1 > operation3 && operation1 > operation4) ? operation1 : (( operation2 > operation3 && operation2 > operation4) ? operation2 : (operation3 > operation4 ? operation3 : operation4));
+    let min=(operation1 < operation2 && operation1 < operation3 && operation1 < operation4) ? operation1 : (( operation2 < operation3 && operation2 < operation4) ? operation2 : (operation3 < operation4 ? operation3 : operation4));
+    
+    console.log("Maximum value :"+max);
+    console.log("Minimum value :"+min);
+}
+    
