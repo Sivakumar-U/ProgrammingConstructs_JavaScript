aa91a4ee45fc15ca7f5ee2f64aa8f0b120375caf
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-28 22:40:47 +0530

    Added: Code to find factors of a given number

--- forLoop.js
+++ forLoop.js
@@ -61,3 +61,27 @@ const prompt = require('prompt-sync')();
         fact = fact * i;
     console.log("Factorial of "+number+" is :"+fact);
 }
+
+// 6) Write a program to compute Factors of a number N using prime factorization method.
+{
+    let number = Number(prompt("Enter number to find its prime factors:"));
+    let count = 0;
+    let flag = 1;
+    for(let i=2; i<number; i++){
+        if(number % i == 0){
+            flag=1;
+            for(let j=2; j<=i/2; j++){
+                if(i % j == 0){
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1){
+                console.log("Prime factor :"+i);
+                count = 1;
+            }
+        }
+    }
+    if(count == 0)
+        console.log("No prime factors..");
+}
