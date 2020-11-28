971d874605cb939be85bce766ae637e915f2d232
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-28 21:07:38 +0530

    Added: Code to read a number and display unit, ten, hundred and thousand

--- IfElse_IfAndElse.js
+++ IfElse_IfAndElse.js
@@ -37,3 +37,18 @@ else
     else
         console.log("Wrong input.");
 }
+
+// 3)Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
+{
+    let number=Number(prompt("Enter the number (1,10,100,..,1000):"));
+    if(number == 1)
+        console.log("Unit.");
+    else if(number == 10)
+        console.log("Ten.");
+    else if(number == 100)
+        console.log("Hundread.");
+    else if(number == 1000)
+        console.log("Thousand.");
+    else
+        console.log("Wrong input.");
+}
