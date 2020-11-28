6f147d02dbce61fd3c78a214e674f3ec3645dbaf
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-28 22:30:29 +0530

    Added: Code to get prime numbers in given range

--- forLoop.js
+++ forLoop.js
@@ -32,3 +32,23 @@ const prompt = require('prompt-sync')();
     else
         console.log(num+" is a prime number.");
 }
+
+// 4) Extend the program to take a range of number as input and output the Prime Numbers in that range.
+{
+    console.log("Enter range to print prime numbers :");
+    let num1=Number(prompt("Enter range of num1:"));
+    let num2=Number(prompt("Enter range of num2:"));
+    let flag=1;
+    let num=0;
+    console.log("Prime numbers are in given range :");
+    for(num = num1+1; num < num2; num++){
+        flag = 1;
+        for(i=2; i<=num/2; i++)
+            if(num % i == 0){
+                flag=0;
+                break;
+            }
+        if (flag == 1)
+            console.log(num);
+    }   
+}
