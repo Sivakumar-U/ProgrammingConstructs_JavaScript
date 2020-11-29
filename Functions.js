177eb4601bde02f6dab15c2099cfc06b5a07be72
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-29 19:32:54 +0530

    Added: Code to check given number is palindrome or not

--- Functions.js
+++ Functions.js
@@ -31,3 +31,23 @@ const prompt = require('prompt-sync')();
         default :   console.log("Wrong input.");
     }
 }
+
+// 2) Write a function to check if the two numbers are Palindromes.
+{
+    let number=Number(prompt("Enter number to check palindrome or not : "));
+
+    function palindromeOrNot(number){
+        let num=number;
+        let reversed=0;
+        while(num != 0){
+            let remainder=num%10;
+            reversed=reversed*10+remainder;
+            num=Math.floor(num/10);
+        }
+        if(number == reversed)
+            console.log(number+" is a palindrome number.");
+        else
+            console.log(number+" is not a palindrome number.");
+    }
+    palindromeOrNot(number);
+}
