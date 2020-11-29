c66ba65175fa44e7dd53d0c0ca8c587cc8ba2d25
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-29 21:39:27 +0530

    Added: Code to generate 10 random 3 didgit numbers to store into array and to find second largest and second smallest number without sorting the array

---
+++ ArrayProblems.js
@@ -0,0 +1,38 @@
+// 1) Write a program in the following steps
+// a. Generates 10 Random 3 Digit number.
+// b. Store this random numbers into a array.
+// c. Then find the 2nd largest and the 2nd smallest element without sorting the array.
+const prompt = require('prompt-sync')();
+{
+    var array=new Array();
+    for (let i=0;i<10;i++){
+        let random=Math.floor(Math.random()*1000);
+        if(random >=100 && random <=999)
+            array[i]=random;
+        else
+            i-=1;
+    }
+    console.log("Generated random numbers into array :"+array);
+    let largest=array[0];
+    let secondLargest=0;
+    for (let i=1;i<=array.length;i++)
+        if(largest < array[i]){
+            secondLargest=largest;
+            largest=array[i];
+        }
+        else if(secondLargest <array[i] && array[i] != largest){
+            secondLargest=array[i];
+        }
+    console.log(" Second largest number : " +secondLargest);
+
+    let smallest=array[0];
+    let secondSmallest=999;
+    for (let i=1;i<=array.length;i++)
+        if(smallest > array[i]){
+            secondSmallest=smallest;
+            smallest=array[i];
+        }else if(secondSmallest > array[i] && array[i] != smallest){
+            secondSmallest=array[i];
+        }
+    console.log(" Second smallest number : " +secondSmallest);
+}
