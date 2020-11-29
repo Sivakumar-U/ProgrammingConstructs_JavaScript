4b4eebdabf0275e2a44a92d12875cb7813415469
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-29 21:45:04 +0530

    Added: Code to sort the array and to find second smallest and largest element

--- ArrayProblems.js
+++ ArrayProblems.js
@@ -36,3 +36,20 @@ const prompt = require('prompt-sync')();
         }
     console.log(" Second smallest number : " +secondSmallest);
 }
+
+// 2) Extend the above program to sort the array and then find the 2nd largest and the 2nd smallest element.
+{
+    var array=new Array();
+    for (let i=0;i<10;i++){
+        let random=Math.floor(Math.random()*1000);
+        if(random >=100 && random <=999)
+            array[i]=random;
+        else
+            i-=1;
+    }
+    console.log("Generated random numbers into array :"+array);
+    array.sort();
+    console.log("Sorted Array :"+array);
+    console.log(" Second largest number : " +array[array.length-2]);
+    console.log(" Second smallest number : " +array[1]);
+}
