e1c34bc758372cc3a285b5d6eaaebb1c88d0c6d5
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-29 21:50:33 +0530

    Added: Code to find prime factors and store all prime factors into an array and display output

--- ArrayProblems.js
+++ ArrayProblems.js
@@ -53,3 +53,29 @@ const prompt = require('prompt-sync')();
     console.log(" Second largest number : " +array[array.length-2]);
     console.log(" Second smallest number : " +array[1]);
 }
+
+// 3) Extend the Prime Factorization Program to store all the Prime Factors of a number n into an array and finally display the output.
+{
+    let number=Number(prompt("Enter number to compute its prime factors:"));
+    let flag=1;
+    var primeFactors=new Array();
+    array_counter=0;
+    for(let i=2;i<=number;i++){
+        if(number% i == 0){
+            flag=1;
+            for(let j=2;j<=i/2;j++){
+                if(i % j == 0){
+                    flag=0;
+                    break;
+                }
+            }
+            if (flag==1){
+                primeFactors[array_counter++]=i;
+            }
+        }
+    }
+    if(number == 1)
+        console.log("1 has no prime factors.");
+    else
+        console.log("Prime factors of "+number+ " are :"+primeFactors);
+}
